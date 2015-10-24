9c490518b2bae9327aa2340bfa79e71f0f8e09b4
cms <cms@t410s>
2015-10-24 08:22:53 +0200

    fix SQLite threadsafe check
    Catches AttributeError for missing sqlite3_threadsafe function in pre-2007
    versions of SQLite (closes #176).
    
    misc:
    Added some docstrings.

--- acdcli/cache/db.py
+++ acdcli/cache/db.py
@@ -13,10 +13,10 @@ from .sync import SyncMixin
 logger = logging.getLogger(__name__)
 
 
-def _regex_match(pattern: str, col: str):
-    if col is None:
+def _regex_match(pattern: str, cell: str) -> bool:
+    if cell is None:
         return False
-    return re.match(pattern, col, re.IGNORECASE) is not None
+    return re.match(pattern, cell, re.IGNORECASE) is not None
 
 
 class NodeCache(QueryMixin, SyncMixin):
@@ -24,8 +24,15 @@ class NodeCache(QueryMixin, SyncMixin):
     _DB_FILENAME = 'nodes.db'
 
     IntegrityCheckType = dict(full=0, quick=1, none=2)
+    """types of start-up SQLite integrity checks"""
 
-    def __init__(self, path='', check=IntegrityCheckType['full']):
+    def __init__(self, path: str='', check=IntegrityCheckType['full']):
+        """Initializes the node cache file in the given path. Performs a migration to highest
+        version if database file exists.
+
+        :param path: path to store database file into, defaults to cwd
+        :param check: type of the integrity check to perform"""
+
         logger.info('Initializing cache with path "%s".' % os.path.realpath(path))
         db_path = os.path.join(path, NodeCache._DB_FILENAME)
 
@@ -34,21 +41,20 @@ class NodeCache(QueryMixin, SyncMixin):
 
         try:
             lib = util.find_library('sqlite3')
-        except OSError:
-            logger.info('Skipping sqlite thread-safety test.')
-        else:
             if lib:
                 dll = CDLL(lib)
                 if dll and not dll.sqlite3_threadsafe():
-                    # http://www.sqlite.org/c3ref/threadsafe.html
+                    # http://www.sqlite.org/c3ref/threadsafe.html (added in 2007?)
                     logger.warning('Your sqlite3 version was compiled without mutexes. '
                                    'It is not thread-safe.')
+        except (OSError, AttributeError):
+            logger.info('Skipping sqlite thread-safety test.')
 
         self.engine = create_engine('sqlite:///%s' % db_path,
                                     connect_args={'check_same_thread': False})
 
-        # check for serialized mode
-
+        # register the named function 'REGEXP' with SQLite
+        # https://docs.python.org/3.2/library/sqlite3.html#sqlite3.Connection.create_function
         listen(self.engine, 'begin',
                lambda conn: conn.connection.create_function('REGEXP', 2, _regex_match))
 
@@ -90,6 +96,9 @@ class NodeCache(QueryMixin, SyncMixin):
             self._migrate(ver)
 
     def integrity_check(self, type_: IntegrityCheckType):
+        """Performs a `self-integrity check
+        <https://www.sqlite.org/pragma.html#pragma_integrity_check>`_ on the database."""
+
         if type_ == NodeCache.IntegrityCheckType['full']:
             r = self.engine.execute('PRAGMA integrity_check;')
         elif type_ == NodeCache.IntegrityCheckType['quick']:
@@ -101,26 +110,26 @@ class NodeCache(QueryMixin, SyncMixin):
                         'You may need to clear the cache if you encounter any errors.')
 
     def dump_table_sql(self):
-        def dump(sql, *multiparams, **params):
+        """Dumps SQL statements for generation of tables in current schema to stdout."""
+        def dump(sql, *args, **kwargs):
             print(sql.compile(dialect=self.engine.dialect))
         engine = create_engine('sqlite://', strategy='mock', executor=dump)
         schema._Base.metadata.create_all(engine, checkfirst=False)
 
     def drop_all(self):
+        """Drops all tables from database."""
         schema._Base.metadata.drop_all(self.engine)
         logger.info('Dropped all tables.')
 
     def _migrate(self, schema: int):
-        """ Migrate database to highest schema
-        :param schema: current (cache file) schema version to upgrade from
-        """
+        """ Migrates database to highest schema.
 
-        migrations = [NodeCache._0_to_1]
+        :param int schema: current (cache file) schema version to upgrade from"""
 
         conn = self.engine.connect()
         trans = conn.begin()
         try:
-            for update_ in migrations[schema:]:
+            for update_ in self.migrations[schema:]:
                 logger.warning('Updating db schema from %i to %i.' % (schema, schema + 1))
                 update_(conn)
                 schema += 1
@@ -137,8 +146,12 @@ class NodeCache(QueryMixin, SyncMixin):
         conn.execute('ALTER TABLE nodes ADD description VARCHAR(500);')
         conn.execute('PRAGMA user_version = 1;')
 
+    migrations = [_0_to_1]
+    """list of all migrations from index -> index+1"""
 
-def remove_db_file(path: str):
+
+def remove_db_file(path: str) -> bool:
+    """Removes database file from passed *path*."""
     db_path = os.path.join(path, NodeCache._DB_FILENAME)
     try:
         os.remove(db_path)
